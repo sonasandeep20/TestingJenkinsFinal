f7edc6bb5d9763fca85edcb8f45d11b4b08e473e
sonasandeep20 <automationsandeep21@gmail.com>
2022-01-02 17:01:37 +0530

    Browser Commit

--- src/test/java/GitTestForGenkins/LaunchingBrowser.java
+++ src/test/java/GitTestForGenkins/LaunchingBrowser.java
@@ -4,21 +4,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class LaunchingBrowser {
-	
+
+	@Parameters("Browser")
 	@Test
-	public void launchbrowser() throws InterruptedException
+	public void launchbrowser(String browserName) throws InterruptedException
 	{
+		
+		System.out.println("Value of browsername is " + browserName);
+		
+		WebDriver driver=null;
+		if(browserName.contains("chrome"))
+		{
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Sandeep\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		 driver = new ChromeDriver();
 		//WebDriver driver = new FirefoxDriver();
+		
+		}
+		else if(browserName.contains("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver","C:\\Users\\Sandeep\\Downloads\\geckodriver-v0.30.0-win64\\geckodriver.exe");
+			
+			 driver = new FirefoxDriver();
+			
+		}
 		driver.get("https://staging.tutist.com/login");
 		driver.manage().window().maximize();
 		Thread.sleep(3000);
-	
-		
 		driver.findElement(By.id("email")).sendKeys("sandeepkumar@mailinator.com");
 		driver.findElement(By.id("password")).sendKeys("123");
 		
